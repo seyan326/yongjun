bbf13b3b58a7e2e181d646d054715d50d8bc167f
yongjun.seo <yongjun.seo@weeds.comm.com>
2023-03-14 22:55:45 +0900

    findview 공부

---
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.jetpack
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import com.example.jetpack.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var btn1 : Button = findViewById(R.id.button_first)
+//        btn1.text = "abcd"
+
+        //Kotlin Extemsions
+        button_first.text = "abcd"
+
+
+    }
+}
